6feb20a1f44e4a75e5de43b78256734b9712f809
unknown <vallejosr@protonmail.com>
2023-07-31 11:35:44 -0300

    Fix: resolviendo conflictos

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -20,16 +19,3 @@ app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'/views/404.html')))
 
 
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
-=======
-const express = require("express");
-const path = require("path");
-const app = express();
-const PORT = 8000;
-app.use(express.static('public'));
-
-app.get("",(req,res)=> res.sendFile(path.join(__dirname,"views","header.html")))
-app.get("/carrito",(req,res)=> res.sendFile(path.join(__dirname,"views","carrito.html")))
-app.get("/error",(req,res)=> res.sendFile(path.join(__dirname,"views","404.html")))
-
-app.listen(PORT, () =>console.log("Server running in http://localhost:" + PORT));
->>>>>>> carrito
