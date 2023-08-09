dd43319ebd69e85f35df820ac7e2852b9122de87
lucaspausin <lucaspausin@gmail.com>
2023-08-09 10:05:26 -0300

    Practicas con Express

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 3030;
 
 /* configuraciones */
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 /* rutas */
 app.get("/", (req, res) =>
@@ -16,9 +17,32 @@ app.get("/carrito", (req, res) =>
 app.get("/productDetail", (req, res) =>
 	res.sendFile(path.join(__dirname, "views", "productDetail.html"))
 );
-app.get("/login", (req, res) =>
-	res.sendFile(path.join(__dirname, "views", "login.html"))
-);
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.post("/login", (req, res) => {
+	const username = req.body.usernameOrEmail;
+	const password = req.body.password;
+
+	// Agrega mensajes de depuración para verificar los valores
+	console.log("Username:", username);
+	console.log("Password:", password);
+
+	// Verificar los datos de inicio de sesión
+	if (
+		(username === "admin" && password === "1234") ||
+		(username === "lucaspausin" && password === "ASTROboy3")
+	) {
+		console.log("Autenticación exitosa");
+
+		// Redirigir a la página de administradores con el nombre de usuario
+		res.redirect(`/administrators/${username}`);
+	} else {
+		res.sendFile(path.join(__dirname, "views", "login.html"));
+	}
+});
+
 app.get("/register", (req, res) =>
 	res.sendFile(path.join(__dirname, "views", "register.html"))
 );
@@ -29,12 +53,11 @@ app.get("/footer", (req, res) =>
 	res.sendFile(path.join(__dirname, "views", "partials", "footer.html"))
 );
 
-app.get("/user/:username", (req, res) => {
-	if (req.params.username === "lucas") {
-		return res.sendFile(path.join(__dirname, "/views/users.html"));
+app.get("/administrators/:username", (req, res) => {
+	if (req.params.username) {
+		return res.sendFile(path.join(__dirname, "views", "users.html"));
 	}
-
-	return res.sendFile(path.join(__dirname, "/views/404.html"));
+	return res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.get("/*", (req, res) =>
@@ -44,4 +67,3 @@ app.get("/*", (req, res) =>
 app.listen(PORT, () =>
 	console.log("Servidor corriendo en http://localhost:" + PORT)
 );
-app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
