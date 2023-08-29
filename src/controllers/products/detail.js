d01741e5bb9b1019d2a9b29fe8f7299afcc033e8
lucaspausin <lucaspausin@gmail.com>
2023-08-29 10:25:15 -0300

    productDetail dinamico, tematicas agregadas, y seccion ultimas unidades

--- src/controllers/products/detail.js
+++ src/controllers/products/detail.js
@@ -4,10 +4,13 @@ module.exports = (req, res) => {
 	const productId = req.params.id; // ID del producto recibido desde la URL
 	const products = readJSON("products.json");
 	const product = products.find((item) => item.id === productId);
-	
+	const lastunits = products.filter((product) => product.stock <= 3);
+
+	const categories = readJSON("categories.json");
 
 	return res.render("productDetail", {
 		product,
-		
+		lastunits,
+		categories,
 	});
 };
