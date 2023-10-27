eebd8d220152ebf4d7fb026134de1484e15707c2
unknown <vallejosr@protonmail.com>
2023-10-26 21:38:56 -0300

    feat: edit & update

--- src/controllers/products/update.js
+++ src/controllers/products/update.js
@@ -5,57 +5,104 @@ const priceFinal = require('../../../public/javascripts/products-function');
 
 module.exports = (req, res) => {
     const errors = validationResult(req);
-    const categories = readJSON('categories.json');
-    const sections = readJSON('sections.json');
-    const products = readJSON('products.json');
-    const { title, category, price, discount, stock, description, section } =
-        req.body;
-
-    const productToUpdate = products.find(
-        (product) => product.id === req.params.id
-    );
-
-    const product = productToUpdate;
 
     if (errors.isEmpty()) {
-        let images = req.files ? req.files.map((file) => file.filename) : [];
+        const {
+            title,
+            categoryId,
+            sectionId,
+            price,
+            discount,
+            stock,
+            description,
+        } = req.body;
 
-        if (images.length > 0) {
-            if (
-                productToUpdate.images &&
-                Array.isArray(productToUpdate.images)
-            ) {
-                productToUpdate.images.forEach((image) => {
-                    const imagePath = `./public/images/productos/${image}`;
-                    if (existsSync(imagePath)) {
-                        unlinkSync(imagePath);
+        db.Product.findByPk(req.params.id, {
+            include: ['images'],
+        }).then((product) => {
+            db.Product.update(
+                {
+                    title: title.trim(),
+                    categoryId,
+                    sectionId,
+                    price,
+                    discount,
+                    stock,
+                    description: description.trim(),
+                },
+                {
+                    where: {
+                        id: req.params.id,
+                    },
+                }
+            )
+                .then(() => {
+                    if (req.files.image) {
+                        product.images.forEach((image) => {
+                            if (
+                                existsSync(
+                                    `./public/images/productos/${
+                                        product.images.find(
+                                            (image) => image.main
+                                        ).file
+                                    }`
+                                )
+                            ) {
+                                unlinkSync(
+                                    `./public/images/productos/${
+                                        product.images.find(
+                                            (image) => image.main
+                                        ).file
+                                    }`
+                                );
+                            }
+                        });
+                        db.Image.destroy({
+                            where: {
+                                productId: req.params.id,
+                                main: true,
+                            },
+                        }).then(() => {
+                            db.Image.create({
+                                file: req.files.image[0].filename,
+                                main: true,
+                                productId: req.params.id,
+                            });
+                        });
                     }
-                });
-            }
-        } else {
-            images = productToUpdate.images;
-        }
-
-        productToUpdate.title = title.trim();
-        productToUpdate.category = category;
-        productToUpdate.price = +price;
-        productToUpdate.discount = +discount;
-        productToUpdate.stock = +stock;
-        productToUpdate.images = images;
-        productToUpdate.section = section;
-        productToUpdate.description = description.trim();
-
-        writeJSON(products, 'products.json');
-        return res.redirect('/users/admin');
-    } else {
-        return res.render('productEdit', {
-            products,
-            categories,
-            sections,
-            errors: errors.mapped(),
-            old: req.body,
-            priceFinal,
-            product,
+                    return res.redirect('/users/admin');
+                })
+                .catch((error) => console.log(error));
         });
+    } else {
+        const id = req.params.id;
+
+        const product = db.Product.findByPk(id, {
+            include: {
+                all: true,
+            },
+        });
+        const categories = db.Category.findAll({
+            order: ['id'],
+        });
+        const sections = db.Section.findAll({
+            order: ['name'],
+        });
+        const products = db.Product.findAll({
+            include: ['category'],
+        });
+
+        Promise.all([product, categories, sections, products])
+            .then(([product, categories, sections, products]) => {
+                return res.render('productEdit', {
+                    ...product.dataValues,
+                    categories,
+                    sections,
+                    products,
+                    priceFinal,
+                    errors: errors.mapped(),
+                });
+            })
+            .catch((error) => console.log(error));
     }
 };
