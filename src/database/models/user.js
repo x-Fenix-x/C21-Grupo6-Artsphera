fc910e1f0fe138d06e4ade990480a6464b638d74
unknown <vallejosr@protonmail.com>
2023-10-26 22:53:36 -0300

    feat: remove

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,27 +1,28 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  User.init({
-    name: DataTypes.STRING,
-    surname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    roleId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+    User.init(
+        {
+            name: DataTypes.STRING,
+            surname: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING,
+            roleId: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'User',
+        }
+    );
+    return User;
 };
