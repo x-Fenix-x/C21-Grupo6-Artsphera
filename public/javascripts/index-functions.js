380df148ae5f8f5c157985d5da20dec46eae9490
unknown <vallejosr@protonmail.com>
2023-10-30 21:58:24 -0300

    fix: profile

--- public/javascripts/index-functions.js
+++ public/javascripts/index-functions.js
@@ -1,6 +1,6 @@
 // CAROUSEL
 
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper('.mySwiper', {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -8,29 +8,29 @@ let swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
 
 // BOTONES SIGUIENTE & ATRÁS
 
-const productoContainer = document.querySelectorAll(".producto-container");
-const antBoton = document.querySelectorAll(".ant-boton");
-const sigBoton = document.querySelectorAll(".sig-boton");
+const productoContainer = document.querySelectorAll('.producto-container');
+const antBoton = document.querySelectorAll('.ant-boton');
+const sigBoton = document.querySelectorAll('.sig-boton');
 
 productoContainer.forEach((card, i) => {
-    const cardWidth = card.querySelector(".producto-card").offsetWidth;
+    const cardWidth = card.querySelector('.producto-card').offsetWidth;
 
-    sigBoton[i].addEventListener("click", () => {
+    sigBoton[i].addEventListener('click', () => {
         card.scrollLeft += cardWidth;
     });
 
-    antBoton[i].addEventListener("click", () => {
+    antBoton[i].addEventListener('click', () => {
         card.scrollLeft -= cardWidth;
     });
 });
