abfcc28270b447134d6e3004411f2911846b3c15
x-fenix-x <vallejosr@protonmail.com>
2023-12-25 17:56:40 -0300

    feat: paginator of admin

--- src/controllers/users/adminPanel.js
+++ src/controllers/users/adminPanel.js
@@ -1,23 +1,38 @@
 const db = require('../../database/models');
+const paginate = require('express-paginate');
 const priceFinal = require('../../../public/javascripts/products-function');
 
-module.exports = (req, res) => {
-    const products = db.Product.findAll({
-        include: ['category', 'section', 'images'],
-    });
-    const categories = db.Category.findAll();
-    const sections = db.Section.findAll({
-        order: ['name'],
-    });
+module.exports = async (req, res) => {
+    try {
+        const { count, rows } = await db.Product.findAndCountAll({
+            limit: req.query.limit,
+            offset: req.skip,
+            include: ['category', 'section', 'images'],
+        });
 
-    Promise.all([categories, sections, products])
-        .then(([categories, sections, products]) => {
-            return res.render('adminPanel', {
-                products,
-                categories,
-                sections,
-                priceFinal,
-            });
-        })
-        .catch((error) => console.log(error));
+        const pagesCount = Math.ceil(count / req.query.limit);
+
+        const categories = await db.Category.findAll();
+        const sections = await db.Section.findAll({
+            order: ['name'],
+        });
+
+        return res.render('adminPanel', {
+            products: rows,
+            categories,
+            sections,
+            priceFinal,
+            pages: paginate.getArrayPages(req)(
+                pagesCount,
+                pagesCount,
+                req.query.page
+            ),
+            paginate,
+            pagesCount,
+            currentPage: req.query.page,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error en el servidor');
+    }
 };
