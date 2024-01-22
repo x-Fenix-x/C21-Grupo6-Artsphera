808d01f5198e9056dad87ead358d9e21f8d46260
x-fenix-x <vallejosr@protonmail.com>
2024-01-21 22:36:57 -0300

    fix: correcciones

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,11 +10,11 @@ module.exports = {
         dialect: 'mysql',
     },
     test: {
-        username: process.env.DB_USERNAME_DEV,
-        password: process.env.DB_PASSWORD_DEV,
-        database: process.env.DB_DATABASE_DEV,
-        host: process.env.DB_HOST_DEV,
-        port: process.env.DB_PORT_DEV,
+        username: process.env.DB_USERNAME_TEST,
+        password: process.env.DB_PASSWORD_TEST,
+        database: process.env.DB_DATABASE_TEST,
+        host: process.env.DB_HOST_TEST,
+        port: process.env.DB_PORT_TEST,
         dialect: 'mysql',
     },
     production: {
