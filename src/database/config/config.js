86f0025805c232c4880e41266c59f83f9ca6f598
x-fenix-x <vallejosr@protonmail.com>
2024-01-21 21:43:43 -0300

    fix: database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,11 +10,11 @@ module.exports = {
         dialect: 'mysql',
     },
     test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        port: 3306,
+        username: process.env.DB_USERNAME_DEV,
+        password: process.env.DB_PASSWORD_DEV,
+        database: process.env.DB_DATABASE_DEV,
+        host: process.env.DB_HOST_DEV,
+        port: process.env.DB_PORT_DEV,
         dialect: 'mysql',
     },
     production: {
